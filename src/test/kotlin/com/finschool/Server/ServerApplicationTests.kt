ba5ceb1990ef710fc9d8e73a6565d2eb479ffad8
Vladick <scazzels@gmail.com>
2024-04-14 00:16:34 +0300

    add implementation and datasource.url

---
+++ src/test/kotlin/com/finschool/Server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finschool.Server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
